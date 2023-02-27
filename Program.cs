6ab103aad31027d4de3f8c8545b8d838a59d3caf
cc211036 <cc211036@hotmail.fr>
2023-02-27 15:44:43 +0100

    init: git repo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSHARP // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The length of the arguments:" + args.Length);
+                    for(int i=0; i < args.Length; i++) {
+            Console.WriteLine(args[i]);
+        }
+        }
+    }
+}
