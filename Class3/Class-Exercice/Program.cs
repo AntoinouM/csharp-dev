70c5170f62851394fbc9adea1af730815516b0ba
cc211036 <cc211036@hotmail.fr>
2023-03-06 14:07:06 +0100

    init: i dont know

---
+++ Class3/Class-Exercice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Class // Note: actual namespace depends on the project name.
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
