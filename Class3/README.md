6999d723f109cb8551c73e60b5f64660d8d9433f
cc211036 <cc211036@hotmail.fr>
2023-03-09 12:00:58 +0100

    init: Class4

--- Class3/README.md
+++ Class3/README.md
@@ -24,37 +24,3 @@
 ### Indexers
 * Research this
   * Use an indexer to retrieve a specific Instance of a Class (Cat.childlist[indexOfChildren]) => return the specified Cat
-
-# Data Structure
-
-1. Ways to roganize data
-
-- Different data structures lead to differtent ways to solve a given problem (algorithm)
-- Different algorithms ,ay give different efficiency (space and time)
-
-## Algorithms and Application
-- Every computer...
-
-## Different datastructure type
-
-### Array / List / Linked List
-
-#### Array
-An array has fixed size, usually quicker to research through
-A List is better to get item from
-
-- Storing data in a sequential memory location
-- Access eahc eleet using integer index
-- Very basic, popular and simple
-```csharp
-Int[] myArray = new int[] {a,b,c,d};
-```
-- Store the elements in a continous way
-
-##### Array problems
-- New insertion and deletion: difficult
-- Need to shift to make space for insertion
-- Need to fill empty positions after deletion
-
-Two extensions exist: Dynamic Array(=List in C#) and Linked List.
-
---
+++ Class4/class-implementation/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace class_implementation
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+
+    // list member is {data; adress} so we need a class that contain both data
+    public class Node 
+    {
+        // Fields
+        private int _data;
+        private Node? _next;
+
+        // Constructors
+        public Node(int d)
+        {
+            _data = d;
+            _next = null;
+        }
+
+        // Getters and Setters
+        public int Data
+        {
+            get { return _data;} // shortend of having a Getage() function with a return
+            set { _data = value;}
+        }
+
+        public Node? Next
+        {
+            get {return _next;}
+            set {_next = value;}
+        }
+
+        // Methods
+
+        // Finalisers
+        ~Node() {
+            //to do on destruction
+        }
+    }
+
+    public class SingleLinkedList
+    {
+        // Fields
+        private Node? _head;
+
+        // Constructors
+        public SingleLinkedList() 
+        {
+            _head = null;
+        }
+
+        // Getters and setters
+        public Node? Head
+        {
+            get { return _head; }
+            set { _head = value; }
+        }
+
+        // Methods
+        // InsertFront
+        // InsertLast
+        // GetLastNode
+        // InsertAfter
+        // FindbyKey
+        // DeleteNodebyKey
+        // PrintList
+
+
+        // Finaliser
+        ~SingleLinkedList() 
+        {
+            // on kill
+        }
+    }
+}
+
