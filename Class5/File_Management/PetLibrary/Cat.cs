b8153845c8a43c831b8d2a87e82ab0dc5e346075
cc211036 <cc211036@hotmail.fr>
2023-03-17 14:25:00 +0100

    add: instance and operator methods

--- Class5/File_Management/PetLibrary/Cat.cs
+++ Class5/File_Management/PetLibrary/Cat.cs
@@ -30,4 +30,30 @@ public class Cat
         System.Console.WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
     }
 
-}
+    /////// Produce kitty function
+    /* static method "multiply" */
+    public static Cat ProduceKitty(Cat cat1, Cat cat2) 
+    {
+        Cat kitty = new Cat 
+        {
+            Name = $"Baby of {cat1.Name} and {cat2.Name}"
+        };
+
+        cat1.Children.Add(kitty);
+        cat2.Children.Add(kitty);
+
+        return kitty;
+    }
+
+    // same thing but use an operator call (see Program)
+    public static Cat operator * (Cat cat1, Cat cat2)
+    {
+        return Cat.ProduceKitty(cat1, cat2);
+    }
+
+    /* Instance method multiply */
+    public Cat ProduceKitty(Cat partner) // can be called by a Cat instance.
+    {
+        return Cat.ProduceKitty(this, partner);
+     }
+ }
