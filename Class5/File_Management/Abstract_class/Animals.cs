13cd94489c0701930e3ee1f99135aea070b21d64
cc211036 <cc211036@hotmail.fr>
2023-03-17 15:50:50 +0100

    init : class Cat

--- Class5/File_Management/Abstract_class/Animals.cs
+++ Class5/File_Management/Abstract_class/Animals.cs
@@ -1,4 +1,24 @@
 namespace Animals 
 {
-    
+    public abstract class Animals
+    {
+        public abstract void Speak();
+        public virtual void Move() // can leave like this or implement body (as below)
+        {
+            System.Console.WriteLine("Move like an animal!");
+        }
+    }
+
+    public class Cat : Animals
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+
+        public Cat(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+
+    }
 }
