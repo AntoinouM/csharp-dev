092889c9603fa92e3ae2afdb1be5c33685008583
cc211036 <cc211036@hotmail.fr>
2023-03-17 16:58:10 +0100

    init: new abstract class file

---
+++ Class5/File_Management/2nd_abstract/Animals.cs
@@ -0,0 +1,40 @@
+namespace _2nd_abstract 
+{
+    public class Cat
+    {
+        public object? food = default(object);
+
+        public string checkedFood(object input)
+        {
+            if(food == null)
+            {
+                return "Expected food is empty.";
+            }
+            else if (food == input)
+            {
+                return "Expected food and input are the same";
+            } else 
+            {
+                return "Expected food and input are NOT the same";
+            }
+        }
+
+    }
+
+    public class GenericCat<T> where T : IComparable
+    {
+        public T? food = default(T?);
+        
+        public string checkFood(T input) {
+            if(food == null) {
+                return "expected food is empty";
+            }
+            else if(food.CompareTo(input) == 0) {
+                return "Expected food and input are the same";
+            }
+            else {
+                return "Expected food and input are NOT the same";
+            }
+        }
+    }
+}
---
+++ Class5/File_Management/2nd_abstract/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _2nd_abstract;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat cat1 = new Cat();
+        cat1.food = 5;
+        System.Console.WriteLine($"Cat food with an integer {cat1.checkedFood(5)}");
+
+        Cat cat2 = new Cat();
+        cat2.food = "fish";
+        System.Console.WriteLine($"Cat food with an integer {cat2.checkedFood("fish")}");
+
+        
+    }
+}
