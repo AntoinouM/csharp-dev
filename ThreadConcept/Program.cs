b888ee56f62038fb72598c5179837376f476972e
cc211036 <cc211036@hotmail.fr>
2023-03-28 15:49:15 +0200

    add: Intro to Thread

--- ThreadConcept/Program.cs
+++ ThreadConcept/Program.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-using Thread;
+using System.Threading;
+using System.Diagnostics;
 
-namespace Thread;
+namespace ThreadConsept;
 class Program
 {
     static void Main(string[] args)
     {
+        Stopwatch timer = Stopwatch.StartNew();
+        System.Console.WriteLine("Running method synchornously on one thread.");
+        MethodA();
+        MethodB();
+        MethodC();
+        timer.Stop();
+        System.Console.WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed");
+    }
 
+    static void MethodA() {
+        Console.WriteLine("Starting Method A...");
+        Thread.Sleep(3000);
+        Console.WriteLine("Finished Method A...");
+    }
+
+    static void MethodB() {
+        Console.WriteLine("Starting Method B...");
+        Thread.Sleep(2000);
+        Console.WriteLine("Finished Method B...");       
+    }
+
+    static void MethodC() {
+        Console.WriteLine("Starting Method C...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Finished Method C...");      
     }
 }
