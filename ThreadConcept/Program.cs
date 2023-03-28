c05e86b5b271468eec6c1d42077d2119162ce2ad
cc211036 <cc211036@hotmail.fr>
2023-03-28 16:58:11 +0200

    add: lock method

--- ThreadConcept/Program.cs
+++ ThreadConcept/Program.cs
@@ -1,36 +1,84 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace ThreadConsept;
 class Program
 {
+    static string _message = ""; // a shared resource
+    private static object conch = new object();
     static void Main(string[] args)
     {
         Stopwatch timer = Stopwatch.StartNew();
-        System.Console.WriteLine("Running method synchornously on one thread.");
-        MethodA();
-        MethodB();
-        MethodC();
+        // System.Console.WriteLine("Running method synchornously on one thread.");
+        // MethodA();
+        // MethodB();
+        // MethodC();
+
+        /* This would allow the operating system to generate an order of execution
+        Task taskA = new Task(MethodA);
+        taskA.Start();
+
+        Task taskB = Task.Factory.StartNew(MethodB);
+        Task taskC = Task.Run(new Action(MethodC));
+        */
+
+        // System.Console.WriteLine("Running method asynchornously on one thread.");
+        // Task taskA = new Task(MethodA);
+        // taskA.Start();
+
+        // Task taskB = Task.Factory.StartNew(MethodB);
+        // Task taskC = Task.Run(new Action(MethodC));
+
+        // Task[] tasks = {taskA, taskB, taskC};
+        // Task.WaitAll(tasks); // tell the program to wait for all the tasks completion
+
+        Task taskA = Task.Factory.StartNew(MethodA);
+        Task taskB = Task.Factory.StartNew(MethodB);
+
+        Task[] tasks = { taskA, taskB };
+        Task.WaitAll(tasks); // the operating system still generate a specific processing order that change
+
+        System.Console.WriteLine(_message);
+
         timer.Stop();
         System.Console.WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed");
     }
 
     static void MethodA() {
-        Console.WriteLine("Starting Method A...");
-        Thread.Sleep(3000);
-        Console.WriteLine("Finished Method A...");
+        lock(conch) {
+            for (int i = 0; i < 5; i++) {
+                Thread.Sleep(500);
+                _message += "A";
+            }
+        }
     }
 
     static void MethodB() {
-        Console.WriteLine("Starting Method B...");
-        Thread.Sleep(2000);
-        Console.WriteLine("Finished Method B...");       
+        lock(conch) {
+            for (int i = 0; i < 5; i++) {
+                Thread.Sleep(500);
+                _message += "B";
+            }
+        }
     }
 
-    static void MethodC() {
-        Console.WriteLine("Starting Method C...");
-        Thread.Sleep(1000);
-        Console.WriteLine("Finished Method C...");      
-    }
+    // static void MethodA() {
+    //     Console.WriteLine("Starting Method A...");
+    //     Thread.Sleep(3000);
+    //     Console.WriteLine("Finished Method A...");
+    // }
+
+    // static void MethodB() {
+    //     Console.WriteLine("Starting Method B...");
+    //     Thread.Sleep(2000);
+    //     Console.WriteLine("Finished Method B...");       
+    // }
+
+    // static void MethodC() {
+    //     Console.WriteLine("Starting Method C...");
+    //     Thread.Sleep(1000);
+    //     Console.WriteLine("Finished Method C...");      
+    // }
 }
